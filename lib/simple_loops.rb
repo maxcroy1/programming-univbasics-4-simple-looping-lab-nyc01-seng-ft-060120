9731710176413c28c9109c855c8d39f695084499
Maxwell Croy <max.croy1@gmail.com>
2020-05-22 14:47:25 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -26,7 +26,7 @@ def return_string_array(array)
   counter = 0
   new_array = []
   while array[counter] do
-    array[counter].to_s 
+    new_array << array[counter].to_s 
     counter += 1 
   end
 end
